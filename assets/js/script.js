fe95c0b38890f2acac00b635de275a33ef8c677e
Faisal L <salluthdev@gmail.com>
2022-05-13 19:38:23 +0700

    Penyesuaian tampilan waktu discount

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
-var waktu = 14;
-setInterval(function() {
-	waktu--;
-	if(waktu < 1) {
-		waktu = 15;
-	} else {
-		document.getElementById("waktu").innerHTML = waktu;
+// let waktu = 15;
+// waktu = localStorage.getItem('waktu');
+// document.getElementById('waktu').innerHTML = waktu;
+// setInterval(function() {
+// 	waktu--;
+// 	if(waktu < 1) {
+// 		waktu = 15;
+// 	} else {
+// 		document.getElementById('waktu').innerHTML = waktu;
+// 	}
+// 	localStorage.setItem('waktu', waktu)
+// }, 1000);
+
+var countDownDate = new Date("06 1, 2024").getTime();
+
+var x = setInterval(function() {
+	var now = new Date().getTime();
+	var distance = countDownDate - now;
+	var days = Math.floor((distance % (1000 * 15 * 60 * 60 * 24)) / (1000 * 60 * 60 * 24));
+
+	document.getElementById("waktu").innerHTML = days;
+
+	if (distance < 0) {
+	clearInterval(x);
+	document.getElementById("waktu").innerHTML = "x";
 	}
-}, 1000 * 60 * 60 * 24);
+});
